cf4b7d41ee17faba301e3c9ec29d76d0eb43e557
William Ye <williamyeny@gmail.com>
2015-09-29 15:53:39 +0000

    added projects

---
+++ js/index.js
@@ -0,0 +1,11 @@
+$( document ).ready(function() {
+
+    $("#drive").click(function() {
+        window.location.href = "nodatabase.html"; 
+    });
+    
+    $("#gaming-club").click(function() {
+        window.location.href = "gamingclub.html"; 
+    });
+    
+});
