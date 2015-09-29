36e8e6ac8bde4805942e29c68b2fe3c155b77e1e
William Ye <williamyeny@gmail.com>
2015-09-29 22:09:56 +0000

    Cool animations for projects (ty material design) and new file paths

--- js/index.js
+++ js/index.js
@@ -1,11 +1,23 @@
 $( document ).ready(function() {
 
-    $("#drive").click(function() {
-        window.location.href = "nodatabase.html"; 
-    });
+//     $("#drive").click(function() {
+//         window.location.href = "nodatabase.html"; 
+//     });
     
-    $("#gaming-club").click(function() {
-        window.location.href = "gamingclub.html"; 
-    });
+    $(".project-sec").hover(function() {
+        $(this).find(".project-text").addClass("project-text-hover"); //moves the text
+        $(this).find(".project-text h2").addClass("project-text-h2-hover"); //fades away the top text
+        $(this).find(".button-container").addClass("button-container-hover"); //brings the buttons to view
+//         $(this).append("<div class=\"button-container\"><a href=\"#\" class=\"button\">VIEW PROJECT</a><a href=\"#\" class=\"button\">SOURCE</a></div>");
+    }, function() {
+        $(this).find(".project-text").removeClass("project-text-hover");
+        $(this).find(".project-text h2").removeClass("project-text-h2-hover");
+        $(this).find(".button-container").removeClass("button-container-hover");
+    
+    })
+    
+//     $("#gaming-club").click(function() {
+//         window.location.href = "gamingclub.html"; 
+//     });
     
 });
