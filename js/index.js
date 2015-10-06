76c945058cece183588a63ffb83ec03c86da7fee
William Ye <williamyeny@gmail.com>
2015-10-06 14:46:48 +0000

    worked on processing, added projects

--- js/index.js
+++ js/index.js
@@ -1,23 +1,43 @@
 $( document ).ready(function() {
 
-//     $("#drive").click(function() {
-//         window.location.href = "nodatabase.html"; 
-//     });
-    
-    $(".project-sec").hover(function() {
-        $(this).find(".project-text").addClass("project-text-hover"); //moves the text
-        $(this).find(".project-text h2").addClass("project-text-h2-hover"); //fades away the top text
-        $(this).find(".button-container").addClass("button-container-hover"); //brings the buttons to view
-//         $(this).append("<div class=\"button-container\"><a href=\"#\" class=\"button\">VIEW PROJECT</a><a href=\"#\" class=\"button\">SOURCE</a></div>");
-    }, function() {
-        $(this).find(".project-text").removeClass("project-text-hover");
-        $(this).find(".project-text h2").removeClass("project-text-h2-hover");
-        $(this).find(".button-container").removeClass("button-container-hover");
-    
+    $(".project-sec").click(function() {
+        //remove classes
+        $(".project-sec").not(this).find(".project-text").removeClass("project-text-hover");
+        $(".project-sec").not(this).find(".project-text h2").removeClass("project-text-h2-hover");
+        $(".project-sec").not(this).find(".button-container").removeClass("button-container-hover");
+        $(".project-sec").not(this).removeClass("project-sec-hover");
+        $(".project-sec").not(this).find(".button").addClass("button-hidden");
+        //toggle classes
+        $(this).find(".project-text").toggleClass("project-text-hover");
+        $(this).find(".project-text h2").toggleClass("project-text-h2-hover");
+        $(this).find(".button-container").toggleClass("button-container-hover");
+        $(this).toggleClass("project-sec-hover");
+        $(this).find(".button").toggleClass("button-hidden");
     })
     
-//     $("#gaming-club").click(function() {
-//         window.location.href = "gamingclub.html"; 
+//     $(".expandable").click(function() {
+//         var projectName = $(this).find("h2").text().replace(/ /g, "_").toLowerCase();
+//         console.log(projectName);
+//         $(".expandable").not(this).remove("canvas");
+//         $(".expandable").not(this).find(".canvas-container").removeClass("canvas-container-hover");
+
+//         $(this).find(".canvas-container").toggleClass("canvas-container-hover");
+//         if ($(this).find(".canvas-container").hasClass("canvas-container-hover")) {
+//             $(this).find(".canvas-container").append("<canvas data-processing-sources=\"pde/" + projectName + ".pde\"></canvas>");
+//         } else {
+//             $(this).find(".canvas-container").remove("canvas");
+//         }
+        
 //     });
+    $(".processing").click(function() {
+        console.log("clickeroony");
+        var projectName = $(this).parent().parent().find("h2").text().replace(/ /g, "_").toLowerCase();   
+        console.log(projectName);
+        var canvasRef = document.createElement('canvas');
+        var p = Processing.loadSketchFromSources(canvasRef, ['/pde/' + projectName + '.pde']);
+        $(this).parent().parent().after("<div class=\"canvas-container\"></div>");
+        $(this).parent().parent().parent().find(".canvas-container").append(canvasRef);
+
+    });
     
 });
