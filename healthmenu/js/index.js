151f81705e13f4004978a9673dafc41d75eae3e3
William Ye <williamyeny@gmail.com>
2015-10-08 16:12:48 +0000

    more health menu stuff

--- healthmenu/js/index.js
+++ healthmenu/js/index.js
@@ -1,10 +1,13 @@
-var foods = ["Stuffed Potato Skins", "French Fries", "Chicken Wings"];
-var calories = ['372','245.8','1,992'];
-var carbs = ['43.3g','32.8g','32g'];
-var fat = ['15.9g','12.00g','165g'];
-var protein = ['17.6g','3.8g','18g'];
+var foods = ["Stuffed Potato Skins", "French Fries", "Chicken Wings","Herb Roasted Prime Rib","Ultimate Fondue","Chicken Fried Steak"];
+var calories = ['372','245.8','1,992','1,400','1,490','758','468','1,660','1,298'];
+var carbs = ['43.3g','32.8g','32g','34.9g','42.6g','40.6g','40g','224g','172g'];
+var fat = ['15.9g','12.00g','165g','71g','40g','30g','13g','51g','31g'];
+var protein = ['17.6g','3.8g','18g','40g','19g','30g','3.6g','0g','0g'];
 
 $(document).ready(function() {
+    var leftOffset = $(".sec:first-child").offset().left;
+    $(".top").css("padding-left", leftOffset)
+    $("h2").css("margin-left", leftOffset);
     $(".nutri").click(function() {
         var p = $(this).parent().parent().parent().offset();
         var inf = $(".info").clone().appendTo(".content");
@@ -21,6 +24,7 @@ $(document).ready(function() {
             }
         }
         inf.find("ul").html('<li><span>Calories: </span>' + calories[num] + '</li><li><span>Carbs: </span>' + carbs[num] + '</li><li><span>Fat: </span>' + fat[num] + '<li><li><span>Protein: </span>' + protein[num] + '</li>');
+        //insert animation code here
     });
     $(document).on('click', '.back', function () {
         $(this).parent().parent().remove();
