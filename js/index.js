0232b949a9091d9326246848e165074e7ad6a8b1
williamyeny <williamyeny@gmail.com>
2016-04-15 22:50:48 -0400

    added email + copy to clipboard

--- js/index.js
+++ js/index.js
@@ -15,20 +15,6 @@ $( document ).ready(function() {
         $(this).find(".button").toggleClass("button-hidden");
     })
     
-//     $(".expandable").click(function() {
-//         var projectName = $(this).find("h2").text().replace(/ /g, "_").toLowerCase();
-//         console.log(projectName);
-//         $(".expandable").not(this).remove("canvas");
-//         $(".expandable").not(this).find(".canvas-container").removeClass("canvas-container-hover");
-
-//         $(this).find(".canvas-container").toggleClass("canvas-container-hover");
-//         if ($(this).find(".canvas-container").hasClass("canvas-container-hover")) {
-//             $(this).find(".canvas-container").append("<canvas data-processing-sources=\"pde/" + projectName + ".pde\"></canvas>");
-//         } else {
-//             $(this).find(".canvas-container").remove("canvas");
-//         }
-        
-//     });
     $(".processing").click(function() {
         console.log("clickeroony");
         $(".canvas-container").remove();
@@ -40,5 +26,12 @@ $( document ).ready(function() {
         $(this).parent().parent().parent().find(".canvas-container").append(canvasRef);
 
     });
+  
+    $("#copy-email").click(function() {
+        $("#copy-email").html("Copied to clipboard!");
+        setTimeout(function() {
+            $("#copy-email").html("williamyeny@gmail.com");
+        },3000);
+    })
     
 });
